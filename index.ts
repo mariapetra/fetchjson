79f706a0834c84a5a17352408ee5da5d049333be
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-10-03 16:35:17 +0800

    make error catching more concise

--- index.ts
+++ index.ts
@@ -20,23 +20,30 @@ axios.get(url).then(response => {
   //we add as Todo as we want the data to match the Todo interface
   const todo = response.data as Todo;
 
-  const ID = todo.id;
+  const id = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
+  const completed = todo.completed;
 
-  console.log(`
-    The todo with ID: ${ID}
-    Has a title of: ${title}
-    Is it finished? ${finished}
-  `);
+  logTodo(id, title, completed);
 });
 
 //run this in your terminal using ` tsc index.html`
 //You will then see a index.js
 
 //to compile:
-  // node index.js
-  // { userId: 1, id: 1, title: 'delectus aut autem', completed: false }
+// node index.js
+// { userId: 1, id: 1, title: 'delectus aut autem', completed: false }
 
 //To run both of these:
-  // ts-node index.ts
+// ts-node index.ts
+
+//now we will make this more concise than it is:
+
+//arrow function
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+};
