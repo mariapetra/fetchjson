4cec061a49ff1dad24ffa392bfe5dc3e615337c1
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-10-03 16:26:01 +0800

    add ts interface to fix errors

--- index.ts
+++ index.ts
@@ -4,7 +4,31 @@ const url = "https://jsonplaceholder.typicode.com/todos/1";
 
 // This is async and you will get a promise in return - it will be called once we get a response from that API
 axios.get(url).then(response => {
-  console.log(response.data);
+  //add TS to fix errors
+  //Response.data has properties of:
+  //id
+  //title
+  //completed
+
+  //interface defines the structure of an object
+  interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+
+  //we add as Todo as we want the data to match the Todo interface
+  const todo = response.data as Todo;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(`
+    The todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `);
 });
 
 //run this in your terminal using ` tsc index.html`
